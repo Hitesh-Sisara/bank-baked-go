87851cc5875ddceeb3cd615c6ffe6369b499a23e
Hitesh Sisara <hitesh.dev@sisara.in>
2024-03-01 01:38:24 +0530

    added transaction api

--- api/server.go
+++ api/server.go
@@ -3,6 +3,8 @@ package api
 import (
 	db "github.com/Hitesh-Sisara/bank-app-go/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
@@ -15,11 +17,16 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+
+	}
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
-	//router.POST("/transfers", server.createTransfer)
-	//router.GET("/transfers/:id", server.getTransfer)
+
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 	return server
